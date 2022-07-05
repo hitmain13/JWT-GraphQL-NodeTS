c138be5812ed4bfd2e6c523ab8f04a892bdb9421
Fabio Matsumoto <hide.fabio123@gmail.com>
2022-07-04 22:20:42 -0300

    feat: apollo server

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,15 +3,15 @@ import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+	type: "postgres",
+	host: "localhost",
+	port: 5432,
+	username: "postgres",
+	password: "postgres",
+	database: "jwt-auth-postgres",
+	synchronize: true,
+	logging: false,
+	entities: [User],
+	migrations: [],
+	subscribers: [],
+});
